9c1a022f7149509562247e005c232d49f44fef7a
Martin Reuter <reuter@mit.edu>
2021-03-23 21:41:04 +0100

    add comment

--- build.sh
+++ build.sh
@@ -11,3 +11,11 @@ for addon in "$@"; do
     echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
   fi
 done
+
+# web says this:
+#docker run --rm -ti --name hassio-builder --privileged \
+#  -v /path/to/addon:/data \
+#  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+#  homeassistant/amd64-builder -t /data --all --test \
+#  -i my-test-addon-{arch} -d local
+
