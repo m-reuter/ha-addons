373c549ff344d282b782af1647e188a3225efbb1
Martin Reuter <reuter@mit.edu>
2021-03-24 16:08:55 +0100

    print archs

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 set -ev
 echo "Addons: $@"
 archs="${ARCHS}"
-echo "Archs: $archs"
 for addon in "$@"; do
-  if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
+#  if [ -z ${TRAVIS_COMMIT_RANGE} ] || git diff --name-only ${TRAVIS_COMMIT_RANGE} | grep -v README.md | grep -q ${addon}; then
     if [ -z "$archs" ]; then
       archs=$(jq -r '.arch // ["armv7", "armhf", "amd64", "aarch64", "i386"] | [.[] | "--" + .] | join(" ")' ${addon}/config.json)
     fi
     echo "Archs: $archs"
+    echo "Test: $TEST"
     docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro -v ~/.docker:/root/.docker -v $(pwd)/${addon}:/data homeassistant/amd64-builder ${archs} -t /data --no-cache ${TEST}
-  else
-    echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
-  fi
+#  else
+#    echo "No change in commit range ${TRAVIS_COMMIT_RANGE}"
+#  fi
 done
 
 # web says this:
