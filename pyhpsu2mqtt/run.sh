d8c27d054dfcc9b26aea2a2a104b5ce1cf743728
Martin Reuter <reuter@mit.edu>
2021-03-24 23:49:14 +0100

    now filling pyhpsu.config

--- pyhpsu2mqtt/run.sh
+++ pyhpsu2mqtt/run.sh
@@ -1,8 +1,37 @@
 #!/usr/bin/with-contenv bashio
 
-if [ ! -f /etc/pyHPSU/pyhpsu.conf ]; then
-    echo "No configuration found... initializing."
-    cp /opt/pyHPSU/etc/pyHPSU/pyhpsu.conf /etc/pyHPSU/pyhpsu.conf
-fi
+CONFIG_PATH=/data/options.json
 
+# Settings for pyhpsu.conf
+PYHPSU_DEVICE="$(bashio::config 'pyhpsu_device')"
+PYHPSU_PORT="$(bashio::config 'pyhpsu_port')"
+PYHPSU_LANG="$(bashio::config 'pyhpsu_lang')"
+MQTT_BROKER="$(bashio::config 'mqtt_broker')"
+MQTT_PORT="$(bashio::config 'mqtt_port')"
+MQTT_USERNAME="$(bashio::config 'mqtt_username')"
+MQTT_PASSWORD="$(bashio::config 'mqtt_password')"
+MQTT_CLIENTNAME="$(bashio::config 'mqtt_clientname')"
+MQTT_PREFIX="$(bashio::config 'mqtt_prefix')"
+MQTT_COMMANDTOPIC="$(bashio::config 'mqtt_commandtopic')"
+CANPI_TIMEOUT="$(bashio::config 'canpi_timeout')"
+JOBS="$(bashio::config 'jobs')"
+
+# Replace in pyhpsu.conf
+sed -i "s/{pyhpsu_device}/${PYHPSU_DEVICE}/g" "pyhpsu.conf"
+sed -i "s/{pyhpsu_port}/${PYHPSU_PORT}/g" "pyhpsu.conf"
+sed -i "s/{pyhpsu_lang}/${PYHPSU_LANG}/g" "pyhpsu.conf"
+sed -i "s/{mqtt_broker}/${MQTT_BROKER}/g" "pyhpsu.conf"
+sed -i "s/{mqtt_port}/${MQTT_PORT}/g" "pyhpsu.conf"
+sed -i "s/{mqtt_username}/${MQTT_USERNAME}/g" "pyhpsu.conf"
+sed -i "s/{mqtt_password}/${MQTT_PASSWORD}/g" "pyhpsu.conf"
+sed -i "s/{mqtt_clientname}/${MQTT_CLIENTNAME}/g" "pyhpsu.conf"
+sed -i "s/{mqtt_prefix}/${MQTT_PREFIX}/g" "pyhpsu.conf"
+sed -i "s/{mqtt_commandtopic}/${MQTT_COMMANDTOPIC}/g" "pyhpsu.conf"
+sed -i "s/{canpi_timeout}/${CANPI_TIMEOUT}/g" "pyhpsu.conf"
+
+
+echo "Initializing pyhpsu configuration ..."
+cp pyhpsu.conf /etc/pyHPSU/pyhpsu.conf
+
+export PYTHONPATH="/usr/lib/python3/dist-packages"
 pyHPSU.py --mqtt_daemon -a -o mqtt
