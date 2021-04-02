87f08eb55ecb0b3e0223322a23d4d7fb07121934
Martin Reuter <reuter@mit.edu>
2021-04-02 18:36:44 +0200

    debug

--- vzlogger2mqtt/run.sh
+++ vzlogger2mqtt/run.sh
@@ -19,7 +19,7 @@ METER_DEVICE2="$(bashio::config 'meter_device2')"
 
 #CLEN =$(jq '.channels | length' /data/options.json)
 CHANNELS=$(jq -r 'if .channels then [.channels[] | "{\n           \"uuid\": \"1\", \n           \"api\": \"null\", \n           \"identifier\": \""+.identifier+"\", \n           \"aggmode\": \""+.aggmode+"\" }" ] | join(",\n         ") else "" end' /data/options.json)
-echo "$CHANNELS"
+#echo "$CHANNELS"
 
 METER_ENABLED2="false"
 if [[ ! -z $METER_DEVICE2 ]]
